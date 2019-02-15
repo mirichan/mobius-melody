f02f11f80799b576c59aa76cf212a19ef8eb5724
Michael Mroz <mmroz@atlassian.com>
2019-02-15 11:22:18 -0800

    missing things

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/components', express.static('bower_components'));
+app.use('/data', express.static('data'));
+
+app.listen('0.0.0.0:8888');
+
+module.exports = app;
